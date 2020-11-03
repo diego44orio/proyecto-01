aaa75e4c92361bc5581093e0fae54159b36fc49e
diego44orio <diego44orio@gmail.com>
2020-11-02 18:07:10 -0600

    programa de calificaciones con if anidados

--- seccion-4 .2/app.js
+++ seccion-4 .2/app.js
@@ -1,20 +1,30 @@
-//crear un algorirtmo,que basado en la nota
-//imprima si el alumno aprueba la materia
-//aprueba 60
-//si el alumno tiene nota menor 50
-//imprimir un mesnaje que digaa:
-//por favor estudie mas
+/* 
+Cree un algoritmo que permita determinar 
+la calificacion en letras de un alumno 
 
-let nota= 30;
+Si la nota se encuentra entre 100 a 90 = A
+Si la nota se encuentra entre 80 a 89 = B
+Si la nota se encuentra entre 70 a 79 = C
+Si la nota se encuentra entre 60 y 69 = D
+Si la nota es inferior a 60 = F
+*/
+let nota = 78;
+let notaletra ="";
+console.log(nota+" es igual a  "+notaletra)
 
-if (nota >= 60) {
-console.log("El alumno aprueba la clase");
-} 
-else {
-    console.log('El alumno NO aprueba la clase');
+if (nota >=90){
+    
+    console.log ("A");
 }
-if (nota < 50){
-
-    console.log("Por favor estudie mas");
+if (nota <=89 && nota >=80){
+    console.log("B")
 }
-console.log('Fin del programa');
+if (nota <=79 && nota >=70){
+    console.log("C")
+}
+if (nota <=69 && nota >=60){
+    console.log("D")
+}
+if (nota <=59){
+    console.log("F")
+}
