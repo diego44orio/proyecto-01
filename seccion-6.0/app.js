f169249382770e0f75be96f12c716280fc4ad7c4
diego44orio <diego44orio@gmail.com>
2020-11-04 16:10:04 -0600

    arreglos de objetos

--- seccion-6.0/app.js
+++ seccion-6.0/app.js
@@ -1,24 +1,22 @@
-function describirPersona ( p ){
 
-        console.log(`${p.nombre} tiene una edad de ${p.edad} mide ${p.estatura}`);
-
-
-}
-
-
-
-
-
-let persona = {
+let persona1 = {
     nombre: "Diego",
     edad: 17,
     estatura: 1.72
-}
+};
+let persona2 = {
+    nombre: "Daniel",
+    edad: 24,
+    estatura: 1.76
+};
+let persona3 = {
+    nombre: "David",
+    edad: 20,
+    estatura: 1.78
+};
 
-describirPersona(persona);
-
-
-
+let personas = [persona1,persona2,persona3];
+console.log(personas);
 
 
 
--- seccion-6.1/app.js
+++ seccion-6.1/app.js
@@ -1,24 +1,25 @@
-function describirPersona ( p ){
 
-        console.log(`${p.nombre} tiene una edad de ${p.edad} mide ${p.estatura}`);
-
-
-}
-
-
-
-
-
-let persona = {
+let persona1 = {
     nombre: "Diego",
     edad: 17,
     estatura: 1.72
+};
+let persona2 = {
+    nombre: "Daniel",
+    edad: 24,
+    estatura: 1.76
+};
+let persona3 = {
+    nombre: "David",
+    edad: 20,
+    estatura: 1.78
+};
+
+let personas = [persona1,persona2,persona3];
+
+// console.log(personas);
+
+for(let i=0;i<=2;i++){
+
+console.log(personas[i].nombre,"--",personas[i].edad);
 }
-
-describirPersona(persona);
-
-
-
-
-
-
