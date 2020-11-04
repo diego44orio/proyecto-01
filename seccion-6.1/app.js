b49f6a24d38ef700ad9aae36ded8d3d6d1544fe7
diego44orio <diego44orio@gmail.com>
2020-11-04 13:59:42 -0600

    mis prmeros objetos

---
+++ seccion-6.1/app.js
@@ -0,0 +1,24 @@
+function describirPersona ( p ){
+
+        console.log(`${p.nombre} tiene una edad de ${p.edad} mide ${p.estatura}`);
+
+
+}
+
+
+
+
+
+let persona = {
+    nombre: "Diego",
+    edad: 17,
+    estatura: 1.72
+}
+
+describirPersona(persona);
+
+
+
+
+
+
