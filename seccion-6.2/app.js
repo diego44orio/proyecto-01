306f4dc6fda382ce872cb70f883a853e2d2c4562
diego44orio <diego44orio@gmail.com>
2020-11-04 18:23:43 -0600

    practicando las clases

--- seccion-6.2/app.js
+++ seccion-6.2/app.js
@@ -1,12 +1,6 @@
 
-
-let carro1 = {
-    marca:"Toyota",
-    tipo: "sedan",
-    puertas: 3,
-    creadoEn:"hoy"
-};
-class Carro {
+class Carro
+ {
 
     constructor(marca,tipo,puertas) {
          this.marca = marca;
@@ -14,11 +8,39 @@ class Carro {
          this.puertas = puertas;
 
          this.creadoEn = "hoy";
+         this.encendido = false;
+         this.gasolina = 100;
+        }
+    encenderCarro(){
+        this.encendido = true;
+        console.log("El carro esta encendido"); 
+    }
+    apagarCarro(){
+        this.encendido = true;
+        console.log("El carro esta apagado");
+
+    }
+    estadoDelCarro(){
+        if(this.encendido ){
+            console.log ("El coche esta enbcendido");
+        }
+        else{
+
+            console.log("El coche esta apagado");
+
+        }
+
+
+    }
+  
+    realizarViaje(consumo) {
+        this.gasolina = this.gasolina - consumo;
+
+        return "Le queda "+ this.gasolina;
+      
     }
 
-
 }
-let carro2 = new Carro("Masda","sedan",2);
+let carro = new Carro("Masda","sedan",2);
 
-console.log(carro1);
-console.log(carro2);
+console.log(carro);
